5c23390e281c9d17726973f1e6e2bc216787600e
Liz Bower <lizzie_beth20@hotmail.com>
2023-07-10 12:09:58 +0000

    second commit

---
+++ MY WORKSPACE/ExpectionHandlingExample/src/com/TryWithMulitpleCatchBlock.java
@@ -0,0 +1,33 @@
+package com;
+
+public class TryWithMulitpleCatchBlock {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		int a=10;
+		int b=1;
+		int abc[]= {100,200,300,400};
+		String info = "10"; // value is 10 but is string consider.
+		try {
+			int result1 = a/b;
+			System.out.println("result "+result1);
+			int result2 = 100/abc[0];
+			System.out.println("result "+result2);
+			System.out.println(info+10);
+			System.out.println(Integer.parseInt(info)+10); //Interger is a type of class which contains ParseInt method
+															// which help to convert to number
+			System.out.println("No Exception");
+		} catch (ArithmeticException e) {
+			System.out.println("Divided by Zero "+e.toString());
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Array Index "+e.toString());
+		} catch (Exception e) {
+			System.out.println("Generic "+e.toString());
+		}
+		
+		System.out.println("Normal Statement");
+		{
+		}}
+	
+		}
+
